28a4a6e87d74538ed513656d22d6b7ea46259904
BramTechxxvi <bramtechxxvi@gmail.com>
2025-05-01 16:46:27 +0100

    new commit

---
+++ Assignment/estore/User.py
@@ -0,0 +1,9 @@
+class User:
+
+    def __init__(self, name, age, house_address, email, phone_number, password):
+        self.name = name
+        self.age = age
+        self.house_address = house_address
+        self.email = email
+        self.phone_number = phone_number
+        self.password = password
--- Assignment/estore/address.py
+++ Assignment/estore/address.py
@@ -1,5 +1,8 @@
 class Address:
 
-    def __init__(self):
-        self.street
-        self.
+    def __init__(self, street, city, state, country, house_number,):
+        self.street = street
+        self.city = city
+        self.state = state
+        self.country = country
+        self.house_number = house_number
